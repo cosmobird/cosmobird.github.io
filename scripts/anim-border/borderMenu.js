6af77a88e990f2466eeac42fb661560e4573f642
firebird <firebird@guy.air>
2016-05-29 02:20:44 +0530

    hack going on

--- scripts/anim-border/borderMenu.js
+++ scripts/anim-border/borderMenu.js
@@ -52,17 +52,6 @@
 			}
 		});
 
-		if( triggerPlay ) {
-			triggerPlay.addEventListener( eventtype, function( ev ) {
-				ev.stopPropagation();
-				ev.preventDefault();
-
-				classie.remove( menu, 'bt-menu-close' );
-				classie.add( menu, 'bt-menu-open' );
-				overlay.addEventListener( eventtype, closeClickFn );
-			});
-		}
-
 	}
 
 	init();
