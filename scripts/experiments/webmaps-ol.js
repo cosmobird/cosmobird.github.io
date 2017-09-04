597b16e3908811f07a9219a47367608619abb3f5
cosmobird <lrmodes@gmail.com>
2017-09-04 19:39:58 +0530

    learning webmaps-ol

--- scripts/experiments/webmaps-ol.js
+++ scripts/experiments/webmaps-ol.js
@@ -1,15 +1,24 @@
+
+/*
+	A Map in Openlayers in made of	:	"layers"
+					:	"view" 		-	to visualize "layers"
+					:	"interactions"	-	to modify content
+					:	"controls"	-	with UI Components
+*/
 var OLmap = new ol.Map( {
 		target: 'omap',
 		layers: [
 			new ol.layer.Tile({
-			source: new ol.source.OSM()
+				source: new ol.source.OSM()
 			})
 			],
 		view: new ol.View({
-		center: ol.proj.fromLonLat([0.00, 0.00]),
-		zoom: 4
+			center: ol.proj.fromLonLat([0.00, 0.00]),
+			zoom: 4
 		})
 	});
+
+/*	TRYING OUT FROM OPENLAYER WORKSHOP - http://openlayers.org/workshop/en/ */
 
 /*ZOOMING TO THE CURRENT USER LOCATION*/
 //-------------------------------------
@@ -19,3 +28,5 @@ navigator.geolocation.getCurrentPosition(function(pos) {
 	const coords = ol.proj.fromLonLat([pos.coords.longitude, pos.coords.latitude]);
 	OLmap.getView().animate({center: coords, zoom: 5});
 });
+
+
