aa6062fe11519c9385f4e1cf0ac554d6fb0e1af5
cosmobird <lrmodes@gmail.com>
2017-11-23 03:01:57 +0530

    modified analytics js to refelect the server side changes, server name to piwik.cosmobird.me

--- scripts/analytics/analytics.js
+++ scripts/analytics/analytics.js
@@ -4,7 +4,7 @@
   _paq.push(['trackPageView']);
   _paq.push(['enableLinkTracking']);
   (function() {
-    var u="//cosmobird.me/piwik/";
+    var u="//piwik.xmagcosmobird.me/";
     _paq.push(['setTrackerUrl', u+'piwik.php']);
     _paq.push(['setSiteId', '1']);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
