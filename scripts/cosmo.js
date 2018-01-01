b5e9e48142696cbcbfab7bca40c0e819ae8993ea
cosmobird <lrmodes@gmail.com>
2018-01-01 19:01:24 +0530

    created cosmo js and added jquery to get mousehover events to get icons names and display it in label span

---
+++ scripts/cosmo.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/*Using JQuery*/
+
+$(document).ready(function() {
+    
+    // get the names from alt tag in the grid list of images and display it to the label
+    
+    $("#grid-small li figure img").hover( 
+	function() {
+	    var names = $(this).attr("alt");
+	    $("#category-label span").text(names);
+	    $("#category-label span").fadeOut(1000);
+	    $("#category-label span").fadeIn(1000);	    
+	},    
+	function() {
+	    $("#category-label span").text(" ");
+	});
+});
