e63af823ee9db8c08e0edec8c1dc6f022cd7b196
firebird <lrmodes@gmail.com>
2018-04-02 19:57:33 +0530

    edited to rectify strange landing page social icons error

--- scripts/cosmo.js
+++ scripts/cosmo.js
@@ -3,16 +3,16 @@
 /*Using JQuery*/
 
 $(document).ready(function() {
-    
+
     // get the names from alt tag in the grid list of images and display it to the label
-    
-    $("#grid-small li figure img").hover( 
+
+    $("#grid-small li figure img").hover(
 	function() {
 	    var names = $(this).attr("alt");
 	    $("#category-label span").text(names);
 //	    $("#category-label span").fadeOut(1000);
-//	    $("#category-label span").fadeIn(1000);	    
-	},    
+//	    $("#category-label span").fadeIn(1000);
+	},
 	function() {
 	    $("#category-label span").text(" ");
 	});
